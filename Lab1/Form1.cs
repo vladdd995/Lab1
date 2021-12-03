9bd1ee0650bdbaadfce3bc4777e888b822deeffa
Vlad YA <v0935021336@mail.ru>
2021-12-03 17:25:02 +0200

    Добавьте файлы проекта.

---
+++ Lab1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Authorization : Form
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Input_Click(object sender, EventArgs e)
+        {
+
+
+            if (Login.Text == "Vlad" && Password.Text == "1234")
+            {
+                Login.BackColor = Color.Green;
+                Password.BackColor = Color.Green;
+                MessageBox.Show("Привіт Влад");
+            }
+            else
+            {
+                Login.BackColor = Color.Red;
+                Password.BackColor = Color.Red;
+                MessageBox.Show("Невірний логін або пароль!");
+            }
+        }
+    }
+}
